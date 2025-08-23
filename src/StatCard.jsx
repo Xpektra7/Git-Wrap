75e4255f6aeabb4e1bd1639e3bd2c153d7b515a2
Xpektra <xpektra7@gmail.com>
2025-08-23 22:56:47 +0100

    Refactor StatCard component layout: Adjust padding and structure for improved readability

--- src/StatCard.jsx
+++ src/StatCard.jsx
@@ -1,10 +1,17 @@
 export default function StatCard({ title, value, subtitle }){
     return(
-        <div className=" flex flex-col p-4 gap-4 rounded border border-(--border)">
+        <div className=" flex flex-col rounded border border-(--border)">
+            <div className="flex flex-col gap-4 p-4">
+
             <h2 className="text-sm text-(--sub-text)">{title}</h2>
             <div className="flex flex-wrap gap-2 items-baseline">
                 <p className="text-(--color) font-bold">{value ? value : "0"}</p>
                 {subtitle && <p className="text-sm text-(--sub-text)">{subtitle}</p>}
+            </div>
+            </div>
+            <div className="flex p-2 px-4 justify-between border-t border-(--border) pt-2">
+                <p className="text-sm text-(--sub-text)">Some additional info</p>
+                <p className="text-sm text-(--sub-text)">&uarr;</p>
             </div>
         </div>
     )
