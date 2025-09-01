1f9e022271113241db88f546a24ef44e37fbd81d
Xpektra <xpektra7@gmail.com>
2025-09-01 15:57:34 +0100

    feat: added getFollowers component

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -10,25 +10,22 @@ import {
   getStarsReceived,
   getStarsGiven,
   getTopLanguages,
-  getLanguagesBreakdown,
   getCommitTimeAnalysis,
   getStreaks,
   getCollaborationCount,
-  getPullRequestsStats
+  getPullRequestsStats,
+  getFollowersGrowth,
 } from "./lib/github";
 import { useEffect, useState } from "react";
 
 export default function Stats({ username, year, theme }) {
+  // Current Year States
   const [repos, setRepos] = useState(0);
   const [commitsInAYear, setCommitsInAYear] = useState(0);
   const [activeRepo, setActiveRepo] = useState(null);
   const [starsReceived, setStarsReceived] = useState(0);
   const [starsGiven, setStarsGiven] = useState(0);
   const [topLanguages, setTopLanguages] = useState([]);
-  const [languagesBreakdown, setLanguagesBreakdown] = useState({
-    breakdown: [],
-    aggregate: {},
-  });
   const [collaborationCount, setCollaborationCount] = useState(0);
   const [commitTimeAnalysis, setCommitTimeAnalysis] = useState({
     hourDistribution: Array(24).fill(0),
@@ -42,6 +39,10 @@ export default function Stats({ username, year, theme }) {
     currentStreak: 0
   });
   const [pullRequestStats, setPullRequestStats] = useState({ });
+  const [followers, setFollowers] = useState(0)
+
+
+  // Previous Year States
   const [prevPullRequestStats, setPrevPullRequestStats] = useState({ });
   const [prevRepos, setPrevRepos] = useState(0);
   const [prevCommitsInAYear, setPrevCommitsInAYear] = useState(0);
@@ -60,34 +61,47 @@ export default function Stats({ username, year, theme }) {
     longestBreak: 0,
     currentStreak: 0
   });
-
+  const [prevFollowers, setPrevFollowers] = useState(0);
   const prevYear = year - 1;
 
   useEffect(() => {
+    // Fetch repos
     fetchRepos(username, year).then(setRepos);
     fetchRepos(username, prevYear).then(setPrevRepos);
-
+    // Total Commits
     getTotalCommits(username, year).then(setCommitsInAYear);
     getTotalCommits(username, prevYear).then(setPrevCommitsInAYear);
-
+    // Active repos
     mostActiveRepo(username, year).then(setActiveRepo);
     mostActiveRepo(username, prevYear).then(setPrevActiveRepo);
-
+    
     starGrazer(username).then(setStars); // legacy
+    // Stars Recieved
     getStarsReceived(username, year).then(setStarsReceived);
-    getStarsGiven(username, year).then(setStarsGiven);
-    getTopLanguages(username, year).then(setTopLanguages);
-    getCommitTimeAnalysis(username, year).then(setCommitTimeAnalysis);
-    getStreaks(username, year).then(setStreakInfo);
-    getCollaborationCount(username, year).then(setCollaborationCount);
-    getPullRequestsStats(username, year).then(setPullRequestStats);
     getStarsReceived(username, prevYear).then(setPrevStarsReceived);
+    // Stars Given
+    getStarsGiven(username, year).then(setStarsGiven);
     getStarsGiven(username, prevYear).then(setPrevStarsGiven);
+    // Top Languages
+    getTopLanguages(username, year).then(setTopLanguages);
     getTopLanguages(username, prevYear).then(setPrevTopLanguages);
+    // Commit Time
+    getCommitTimeAnalysis(username, year).then(setCommitTimeAnalysis);
     getCommitTimeAnalysis(username, prevYear).then(setPrevCommitTimeAnalysis);
+    // Streaks
+    getStreaks(username, year).then(setStreakInfo);
     getStreaks(username, prevYear).then(setPrevStreakInfo);
+    // Collaboartion
+    getCollaborationCount(username, year).then(setCollaborationCount);
     getCollaborationCount(username, prevYear).then(setPrevCollaborationCount);
+    // Pull requests
+    getPullRequestsStats(username, year).then(setPullRequestStats);
     getPullRequestsStats(username, prevYear).then(setPrevPullRequestStats);
+    // Followers
+    getFollowersGrowth(username,year).then(setFollowers);
+    getFollowersGrowth(username,prevYear).then(setPrevFollowers);
+    
+    
   }, [username]);
 
 
@@ -234,10 +248,25 @@ export default function Stats({ username, year, theme }) {
           growth='ignore'
         />
 
-
+        {/* Followers */}
+        <StatCard 
+          title="Followers"
+          value={followers.count}
+          prevValue={prevFollowers.count}
+          growth={
+            prevFollowers.count !== 0
+              ? Math.round(
+                  ((followers.count - prevFollowers.count) /
+                    prevFollowers.count) *
+                    100
+                )
+              : 0
+          }
+        />
       </div>
 
-      <div className="grid grid-cols-1 mt-8 h-120 md:h-60 grid-rows-2 md:grid-cols-2 gap-4">
+
+      <div className="grid grid-cols-1 mt-8 h-240 md:h-120 grid-rows-4 md:grid-cols-2 gap-4">
         <HourlyCommits commitTimeAnalysis={commitTimeAnalysis} prevCommitTimeAnalysis={prevCommitTimeAnalysis} theme={theme} year={year} prevYear={prevYear} />
 
         <DailyCommits username={username} theme={theme} year={year} />
