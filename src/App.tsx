6918846b1b3ce9d2381303770ab9d32ff87f28a5
Xpektra <xpektra7@gmail.com>
2025-11-21 00:28:31 +0100

    feat: add vercel analytics and removed redudancy

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { useSearchParams } from "react-router-dom";
 import Stats from "./Stats";
 import UserNotFound from "./components/UserNotFound";
 import LandingPage from "./components/LandingPage";
-import MetaTags from "./components/MetaTags";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App(): React.ReactElement {
   const [name, setName] = useState<string>("");
--- src/Stats.tsx
+++ src/Stats.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import { useEffect, useState } from "react";
 import type { SetURLSearchParams } from "react-router-dom";
 import StatCard from "./StatCard";
